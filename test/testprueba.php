52f32ba577bbef0de74c23ac7bac3da380f9a8d8
AndresYRodriguez <ayrodriguez035@misena.edu.co>
2022-09-28 11:33:10 -0500

    version 3.8

--- test/testprueba.php
+++ test/testprueba.php
@@ -2,8 +2,8 @@
     use PHPUnit\Framework\TestCase;
     class testprueba extends TestCase {
         public function testCone() {
-            include(__DIR__.'/abdomen.php'); //Archivo a probar
-            $this->assertIsString('<span class="title">Health_up</span>'); //Añadir y validar el testeo.
+            include(__DIR__.'/config/config.php'); //Archivo a probar
+            $this->assertIsNotBool($con); //Añadir y validar el testeo.
         }
     }
     //Para probar fragmentos de etiquetas HTML: assertStringContainsString. Usar ob_start() antes de include() y ob_get_clean() como segundo parámetro.
